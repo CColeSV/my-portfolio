e1da2dd37d81484b833a94746f35228065ce92cd
Christian Coleman <christian.c2508@gmail.com>
2020-02-19 23:07:00 -0500

    updated image & added command to package.json

--- services/portfolio-data.service.tsx
+++ services/portfolio-data.service.tsx
@@ -147,7 +147,7 @@ export default class PortfolioDataService {
         detailed:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
-      img: "img-4-4.jpg",
+      img: "img-7-2.jpg",
       tagList: [SkillEnum.ts, SkillEnum.react, SkillEnum.html5, SkillEnum.css3],
       url: {
         github: "https://github.com/HarveyD/seng-portfolio",
